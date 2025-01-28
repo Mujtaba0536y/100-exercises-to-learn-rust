01feeb68527ff452862a6b68c239620af7d5ddd0
Ali Mujtaba <153592163+Mujtaba0536y@users.noreply.github.com>
2025-01-28 17:58:11 +0500

    Update lib.rs

--- exercises/03_ticket_v1/04_visibility/src/lib.rs
+++ exercises/03_ticket_v1/04_visibility/src/lib.rs
@@ -1,12 +1,12 @@
 mod ticket {
-    struct Ticket {
-        title: String,
-        description: String,
-        status: String,
+    pub struct Ticket {
+        pub  title: String,
+        pub description: String,
+        pub  status: String,
     }
 
     impl Ticket {
-        fn new(title: String, description: String, status: String) -> Ticket {
+        pub  fn new(title: String, description: String, status: String) -> Ticket {
             if title.is_empty() {
                 panic!("Title cannot be empty");
             }
@@ -34,6 +34,7 @@ mod ticket {
 
 // TODO: **Exceptionally**, you'll be modifying both the `ticket` module and the `tests` module
 //  in this exercise.
+
 #[cfg(test)]
 mod tests {
     // TODO: Add the necessary `pub` modifiers in the parent module to remove the compiler
@@ -55,7 +56,7 @@ mod tests {
         //
         // TODO: Once you have verified that the below does not compile,
         //   comment the line out to move on to the next exercise!
-        assert_eq!(ticket.description, "A description");
+//        assert_eq!(ticket.description, "A description");
     }
 
     fn encapsulation_cannot_be_violated() {
@@ -68,10 +69,10 @@ mod tests {
         //
         // TODO: Once you have verified that the below does not compile,
         //   comment the lines out to move on to the next exercise!
-        let ticket = Ticket {
-            title: "A title".into(),
-            description: "A description".into(),
-            status: "To-Do".into(),
-        };
+//        let ticket = Ticket {
+//           title: "A title".into(),
+//            description: "A description".into(),
+//            status: "To-Do".into(),
+//        };
     }
 }
