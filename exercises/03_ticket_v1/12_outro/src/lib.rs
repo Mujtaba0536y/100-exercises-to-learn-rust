6c749a678147142367ef23b4001bdb1dd7735652
Ali Mujtaba <153592163+Mujtaba0536y@users.noreply.github.com>
2025-02-01 21:18:43 +0500

    Update lib.rs
    Creating a Public Struct named Order and implementing Order to validate the given conditions for product_name, quantity and unit price.

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,43 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order{
+    pub product_name: String,
+    pub quantity: u32,
+    pub unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order{
+        validiate_product_name(&product_name);
+        validiate_quantity(&quantity);
+        validiate_unit_price(&unit_price);
+
+        Order{
+            product_name,
+            quantity,
+            unit_price
+        }
+    }
+}
+
+fn validiate_product_name(product_name: &String){
+    if product_name.is_empty(){
+        panic!("Prodcut name should not be empty.")
+    }
+    if product_name.len() >=300{
+        panic!("Product name should not be more then 300 bytes.")
+    }
+}
+
+fn validiate_quantity(quantity: &u32){
+    if quantity == &0{
+        panic!("Quantity should not be zero.")
+    }
+}
+
+fn validiate_unit_price(unit_price: &u32){
+    if unit_price == &0 {
+        panic!("Unit price should not be zero")
+    }
+}
